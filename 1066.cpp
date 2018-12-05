56ebb03321e4d00fc458329f2b22c40466168152
Pip <jingbang.liu@outlook.com>
2018-12-05 11:33:45 +0800

    AVL TREE

--- 1066.cpp
+++ 1066.cpp
@@ -1,105 +1,117 @@
-
-#include<iostream>
-#include<cstdio>
-#include<cmath> //abs需要
+#include <iostream>
+#include <cstdio>
 using namespace std;
 
-struct Node{
-    int value;
-    Node *lchild;
-    Node *rchild;
-    int height;
-    Node(int v) :value(v), lchild(NULL), rchild(NULL), height(0){}
-    Node() :lchild(NULL), rchild(NULL){}
-};
+typedef struct node
+{
+    int data;
+    node *lc, *rc;
+} treeNode;
 
-int getHeight(Node *t)
+int findDeep(treeNode *r)
 {
-    if (t == NULL)return -1;
-    else return t->height;
-}
-int max(int a, int b){
-    return a > b ? a : b;
-}
-
-Node *SingleRotateLeft(Node *k2)
-{//左左的情况
-    Node *k1;
-    k1 = k2->lchild;
-    k2->lchild = k1->rchild;
-    k1->rchild = k2;
-    k2->height = max(getHeight(k2->lchild), getHeight(k2->rchild)) + 1;
-    k1->height = max(getHeight(k1->lchild), getHeight(k1->rchild)) + 1;
-    return k1;
-}
-Node *SingleRotateRight(Node *k2)
-{//右右的情况
-    Node *k1;
-    k1 = k2->rchild;
-    k2->rchild = k1->lchild;
-    k1->lchild = k2;
-    k2->height = max(getHeight(k2->lchild), getHeight(k2->rchild)) + 1;
-    k1->height = max(getHeight(k1->lchild), getHeight(k1->rchild)) + 1;
-    return k1;
-}
-Node *DoubleRotateLR(Node *k3)
-{//左右的情况
-    k3->lchild = SingleRotateRight(k3->lchild);
-    return SingleRotateLeft(k3);
-}
-Node *DoubleRotateRL(Node *k3)
-{//右左的情况
-    k3->rchild = SingleRotateLeft(k3->rchild);
-    return SingleRotateRight(k3);
-}
-bool isBalanced(Node *left, Node *right)
-{
-    return abs(getHeight(left) - getHeight(right)) < 2;
-}
-Node *insert(int v, Node *root)
-{
-    if (root == NULL)
+    if(r==NULL)
     {
-        root = new Node(v);
-        return root;
-    }
-    else if (v > root->value)
-    {
-        root->rchild = insert(v, root->rchild);
-        if (!isBalanced(root->lchild, root->rchild))
-        {
-            if (v > root->rchild->value)
-                root = SingleRotateRight(root);
-            else
-                root = DoubleRotateRL(root);
-        }
+        return 0;
     }
     else
     {
-        root->lchild = insert(v, root->lchild);
-        if (!isBalanced(root->lchild, root->rchild))
-        {
-            if (v < root->lchild->value)
-                root = SingleRotateLeft(root);
-            else
-                root = DoubleRotateLR(root);
-        }
+        int ll, rr;
+        ll = findDeep(r->lc);
+        rr = findDeep(r->rc);
+        return (ll < rr ? rr : ll) + 1;
     }
-    root->height = max(getHeight(root->lchild), getHeight(root->rchild)) + 1;
-    return root;
+}
+treeNode *Right(treeNode *r)
+{
+    treeNode *temp = r->lc;
+    r->lc = temp->rc;
+    temp->rc = r;
+    return temp;
+}
+treeNode *Left(treeNode *r)
+{
+    treeNode *temp = r->rc;
+    r->rc = temp->lc;
+    temp->lc = r;
+    return temp;
+}
+treeNode *RightLeftR(treeNode *r)
+{
+    r->rc = Right(r->rc);
+    r = Left(r);
+    return r;
+}
+treeNode *LeftRightR(treeNode *r)
+{
+    r->lc = Left(r->lc);
+    r = Right(r);
+    return r;
+}
+treeNode *insert(int num,treeNode *r)
+{
+    if(r==NULL)
+    {
+        r = new treeNode();
+        r->data = num;
+        r->lc = NULL;
+        r->rc = NULL;
+        return r;
+    }
+    else
+    {
+        if(num<r->data)
+        {
+            int ll, rr;
+            r->lc = insert(num, r->lc);
+            ll = findDeep(r->lc);
+            rr = findDeep(r->rc);
+            if(ll-rr>=2)
+            {
+                if(num<r->lc->data)
+                {
+                    r = Right(r);
+                }
+                else
+                {
+                    r = LeftRightR(r);
+                }
+            }
+        }
+        else
+        {
+            int ll, rr;
+            r->rc = insert(num, r->rc);
+            ll = findDeep(r->lc);
+            rr = findDeep(r->rc);
+            if(rr-ll>=2)
+            {
+                if(num>r->rc->data)
+                {
+                    r = Left(r);
+                }
+                else
+                {
+                    r = RightLeftR(r);
+                }
+            }
+        }
+        return r;
+    }
 }
 
-int main(void)
+int main()
 {
-    int n;
-    scanf("%d", &n);
-    Node *root = NULL;
-    int tmp;
-    for (int i = 0; i < n; i++)
+    int N;
+    treeNode *root;
+    root = NULL;
+    cin >> N;
+    for (int i = 0; i < N;i++)
     {
-        scanf("%d", &tmp);
-        root = insert(tmp, root);
+        int t;
+        cin >> t;
+        root = insert(t, root);
     }
-    printf("%d\n", root->value);
+    cout << root->data << endl;
     return 0;
 }
