1a3664b4b113739c1ffbca939070f2c5d1522ed1
Pip <jingbang.liu@outlook.com>
2018-12-05 19:51:07 +0800

    passed

--- 1096.cpp
+++ 1096.cpp
@@ -4,22 +4,55 @@
 #include <cmath>
 using namespace std;
 
-queue<int> ans;
+queue<long long> ans;
 int main()
 {
-    long N;
-    long chushu = 1;
+    long long N;
+    long long product = 1;
+    long long otherNum;
+    int maxSize = 0,lastNum;
     cin >> N;
-    for (int i = 2; i < N;i++)
+    for (int i = 1; i <= sqrt(N)+1;)
     {
-        if(N%(chushu*i)==0)
+        if(N%product==0)
         {
+            if(ans.size()>maxSize)
+            {
+                maxSize = ans.size();
+                lastNum = ans.back();
+            }
+            i++;
             ans.push(i);
+            product *= i;
+            
+            
         }
         else
         {
-            
+            product /= ans.front();
+            ans.pop();
         }
+   
+    }
+    if(maxSize==0)
+    {
+        cout << "1" << endl
+             << N << endl;
+    }
+    else
+    {
+        product = 1;
+        for (int i = 0; i < maxSize;i++)
+        {
+            product *= (lastNum - i);
+        }
+
+        cout << maxSize << endl;
+        for (long long i = lastNum - maxSize + 1; i < lastNum;i++)
+        {
+            cout << i << "*";
+        }
+        cout << lastNum << endl;
     }
     return 0;
 } 
